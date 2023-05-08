3295f5a0e19d438063aeb6278bfcbae7b326e933
Robert Carbajal <robert@holycow2.com>
2023-05-07 21:09:43 -0500

    - Updated track data to include public served URLs

--- api/brain_fm_api/src/data/tracks.ts
+++ api/brain_fm_api/src/data/tracks.ts
@@ -5,51 +5,51 @@ export const TrackListing: Record<'focus' | 'relax' | 'sleep', Track[]> = {
         {
             title: 'Aspen Migration',
             length: 60,
-            url: '',
+            url: '/audio/focus/aspen_migration.mp3',
         },
         {
             title: 'Cyan',
             length: 60,
-            url: '',
+            url: '/audio/focus/cyan.mp3',
         },
         {
             title: 'Treasure Map',
             length: 60,
-            url: '',
+            url: '/audio/focus/treasure_map.mp3',
         },
     ],
     'relax': [
         {
             title: 'Bathed in Neon',
             length: 60,
-            url: '',
+            url: '/audio/focus/bathed_in_neon.mp3',
         },
         {
             title: 'Low Tide',
             length: 60,
-            url: '',
+            url: '/audio/focus/low_tide.mp3',
         },
         {
             title: 'Vapor',
             length: 60,
-            url: '',
+            url: '/audio/focus/vapor.mp3',
         },
     ],
     'sleep': [
         {
             title: 'Dark Moon',
             length: 60,
-            url: '',
+            url: '/audio/focus/dark_moon.mp3',
         },
         {
             title: 'Lunaris',
             length: 60,
-            url: '',
+            url: '/audio/focus/lunaris.mp3',
         },
         {
             title: 'Moonflower',
             length: 60,
-            url: '',
+            url: '/audio/focus/moonflower.mp3',
         },
     ],
 };
